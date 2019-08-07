10d5e13a2a4a6e8acb92b1638a3b6fd61a2ff74f
maeda <takanori@stroly.jp>
2019-08-07 22:50:27 +0900

    諸々機能をお試し

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,10 @@ const mymap = L.map('map').setView(
   12,
 );
 
-L.tileLayer(
+/**
+ * Layer
+ */
+const streetsLayer = L.tileLayer(
   'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
   {
     attribution:
@@ -13,8 +16,49 @@ L.tileLayer(
     maxZoom: 18,
     id: 'mapbox.streets', // mapbox.streets | mapbox.satellite
     accessToken: 'your.mapbox.access.token',
-  } as any,
-).addTo(mymap);
+  },
+);
+const satelliteLayer = L.tileLayer(
+  'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.satellite', // mapbox.streets | mapbox.satellite
+    accessToken: 'your.mapbox.access.token',
+  },
+);
+
+/**
+ * Bounds
+ */
+const bounds = new L.LatLngBounds([35.09432984468491, 139.04267643565133], [35.99432984468491, 139.94267643565133]);
+
+/**
+ * Map
+ */
+const mymap = L.map('map').setView(
+  [35.69432984468491, 139.74267643565133],
+  12,
+).fitBounds(bounds).addLayer(streetsLayer);
+
+/**
+ * 現在地を取得ボタンを表示する
+ */
+mymap.locate({
+  watch: true,
+  enableHighAccuracy: true,
+});
+mymap.on('locationfound', (data) => {
+  console.log(`現在地を取得しました: ${data.latlng.lat}, ${data.latlng.lng}`);
+});
+
+mymap.invalidateSize();
+
+L.control.layers({
+  street: streetsLayer,
+  satellite: satelliteLayer,
+}).addTo(mymap);
 
 /**
  * Marker
